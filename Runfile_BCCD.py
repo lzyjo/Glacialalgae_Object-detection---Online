d8cdb1795924ee0107a2c27e41270753336b7f66
lzyjo <lmzyjoey@gmail.com>
2025-01-27 06:41:54 +0000

    datset set up and runfile addition

---
+++ Runfile_BCCD.py
@@ -0,0 +1,100 @@
+
+
+# Checking and setting cwd 
+import json
+import os
+
+## Check the current working directory
+cwd = os.getcwd()
+print(f"Current working directory: {cwd}")
+
+expected_cwd = r"C:\Users\me20332\OneDrive - University of Bristol\MscR\GitHub\Testing_different_datasets"
+if cwd != expected_cwd:
+    print("Current working directory is not the Testing_different_datasets directory")
+    os.chdir(r"C:\Users\me20332\OneDrive - University of Bristol\MscR\GitHub\Testing_different_datasets") # Set the current working directory if needed
+    print("Current working directory has been set to the Testing_different_datasets directory")
+else:
+    print("Current working directory is the Testing_different_datasets directory")
+
+
+# Run the BCCD_utils.py file
+os.system('python BCCD_utils.py')
+
+
+## Check if the data exists and extract it if not
+from BCCD_utils import initialize_bccd_trainvaltest_data
+
+# direct paths to the BCCD dataset and make sure they are absolute
+train_path = os.path.abspath(r"BCCD.v3\Original\train")
+test_path = os.path.abspath(r"BCCD.v3\Original\test")
+valid_path = os.path.abspath(r"BCCD.v3\Original\valid")
+
+
+initialize_bccd_trainvaltest_data(train_path=train_path,
+                                  test_path=test_path,
+                                  valid_path=valid_path)
+
+
+# Structuring of the BCCD dataset (according to VOC) 
+from BCCD_utils import setup_annotations_for_dataset, setup_images_for_dataset
+
+src_dir = os.path.abspath(r"BCCD.v3\Original")
+dest_dir = os.path.abspath(r"BCCD.v3\Annotations")
+setup_annotations_for_dataset(src_dir= src_dir, dest_dir=dest_dir)
+
+src_dir = os.path.abspath(r"BCCD.v3\Original")
+dest_dir = os.path.abspath(r"BCCD.v3\Images")
+setup_images_for_dataset(src_dir= src_dir, dest_dir=dest_dir)
+
+
+# Creating datalists for the train, val and test data
+from BCCD_utils import parse_annotation, create_data_lists
+
+# Label map
+BCCD_labels = ('WBC', 'RBC', 'Platelets')
+label_map = {k: v + 1 for v, k in enumerate(BCCD_labels)}
+label_map['background'] = 0
+rev_label_map = {v: k for k, v in label_map.items()}  # Inverse mapping
+
+# Color map for bounding boxes of detected objects from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8']
+label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
+
+create_data_lists(annotation_path=r"BCCD.v3\Annotations",
+                  train_path=train_path,
+                  test_path=test_path,
+                  valid_path=valid_path,
+                  output_folder=cwd)
+
+
+# Check if model is already trained and present 
+if os.path.exists('checkpoint_ssd300.pth'):
+    print('Model already trained: checkpoint_ssd300.pth present in cwd')
+else:
+    print('Model not trained or present: checkpoint_ssd300.pth not present in cwd')
+
+
+# Training the model
+from BCCD_dataset import BCCDDataset
+
+data_folder = './'  # folder with data files
+os.system('python BCCD_train.py')
+
+# Evaluate the model
+
+## Check if model is already trained and present 
+if os.path.exists('checkpoint_ssd300.pth.tar'):
+    print('Model already trained: checkpoint_ssd300.pth present in cwd')
+else:
+    print('Model not trained or present: checkpoint_ssd300.pth not present in cwd')
+
+os.system('python BCCD_eval.py')
+
+
+# Inference 
+
+os.system('python BCCD_detect.py')
+
+import BCCD_detect # want to be able to directly use the detect function and change the file 
+# directly to use the cnn to annotate the test images  
+
