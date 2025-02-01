78832a597f12631f539953b1b24e3e9f0384e7f2
lzyjo <lmzyjoey@gmail.com>
2025-02-01 04:38:31 +0000

    Refactor label extraction in Runfile_BCCD.py to improve clarity and consistency

--- Runfile_BCCD.py
+++ Runfile_BCCD.py
@@ -24,8 +24,8 @@ from BCCD_utils import create_data_lists
 # Label map
 label_classes_path = os.path.abspath(r"label_classes.csv") # Load label classes from CSV
 label_classes_df = pd.read_csv(label_classes_path) # read csv, and prevent pd from treating first row as a header row
-labels = tuple(label_classes_df.iloc[:, 0].tolist())  # Derive labels from the first column of the CSV
-label_map = {k: v + 1 for v, k in enumerate(labels)}
+label_classes = tuple(label_classes_df.iloc[:, 0].tolist())  # Derive labels from the first column of the CSV
+label_map = {k: v + 1 for v, k in enumerate(label_classes)}
 rev_label_map = {v: k for k, v in label_map.items()}  # Inverse mapping
 
 create_data_lists(train_annotation_path=r'GA_Dataset/Split/train/annotations',
